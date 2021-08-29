11033c3d7d242dac43c9aeb07f900c90b61a4757
Stan <xuan9230@gmail.com>
2021-08-29 17:48:38 +1000

    [backend]: enable CORs

--- lvr-service/src/libs/apiGateway.ts
+++ lvr-service/src/libs/apiGateway.ts
@@ -20,5 +20,10 @@ export const formatJSONResponse = (
   return {
     statusCode,
     body: JSON.stringify(response),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
   }
 }
